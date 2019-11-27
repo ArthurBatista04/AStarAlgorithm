2853ccb3c8feeefe13df3d6a1c435c2725b5796e
Arthur <arthurbatista04@outlook.com>
2019-11-27 08:45:11 -0300

    finished

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+from heapq import heappush, heappop
 from table import *
-from table import table
 from node import Node
+
+
+def aStarAlgorithm(S):
+    F = {}
+    A = {}
+    heap = []
+    A.update({str(S.table.numbers): S})
+    heappush(heap, (S.data, S))
+    v = S
+    while True:
+        v = heappop(heap)[1]
+        if str(v.table.numbers) in A:
+            A.pop(str(v.table.numbers))
+        F.update({str(v.table.numbers): S})
+        if v.table.numberOfCorrectPieces == 15:
+            finished = True
+            break
+        successors = v.genSuccessors()
+        for child in successors:
+            if str(child.table.numbers) in A and child.data < A[str(child.table.numbers)].data:
+                A.pop(str(child.table.numbers))
+
+            if not str(child.table.numbers) in A and not str(child.table.numbers) in F:
+                A.update({str(child.table.numbers): child})
+                heappush(heap, (child.data, child))
+    if finished:
+        return (v.gValue)
 
 
 def parseInput(numbers):
@@ -10,12 +37,9 @@ def parseInput(numbers):
 def main():
     numbers = input()
     parsedNumbers = parseInput(numbers)
-    x = table(parsedNumbers)
-    y = Node(x)
-    print()
-    y.table.format()
-    print()
-    y.genSuccessors()
+    firstTable = table(parsedNumbers)
+    S = Node(table=firstTable)
+    print(aStarAlgorithm(S))
 
 
 if (__name__ == "__main__"):
--- node.py
+++ node.py
@@ -1,211 +1,25 @@
 from copy import deepcopy
-from table import table
-import numpy as np
+from table import *
 
 
-class NodeKey():
-    def __init__(self, value, name=None):
-        self.name = name
-        self.value = value
-
-    def __lt__(self, other):
-        return self.value < other.value or (self.value == other.value and self.name < other.name)
-
-    def __le__(self, other):
-        return self < other or self == other
-
-    def __eq__(self, other):
-        return self.value == other.value and self.name == other.name
-
-    def __ne__(self, other):
-        return self.value != other.value or self.name != other.name
-
-    def __gt__(self, other):
-        return self.value > other.value or (self.value == other.value and self.name > other.name)
-
-    def __ge__(self, other):
-        return self > other or self == other
-
-    def __str__(self):
-        if self.name is None:
-            return str(self.value)
-        else:
-            return str(self.value) + "," + str(self.name)
-
-
-class Node():
-    def __init__(self,  table, gValue=0, name=None):
+class Node:
+    def __init__(self, table, gValue=0, realParent=None):
         self.table = table
         self.gValue = gValue
         self.heuristic = self.h1()
-        self.key = NodeKey(self.gValue + self.heuristic, name)
-        self.value = self.gValue + self.heuristic
+        self.data = self.gValue + self.heuristic
         self.parent = None
+        self.left = None
+        self.right = None
+        self.bf = 0
         self.realParent = None
-        self.left_child = None
-        self.right_child = None
-        self.height = 0
 
-    def __str__(self):
-        return str(self.key)
+    def __gt__(self, other):
+        if not isinstance(other, Node):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
 
-    def next(self):
-        """ Returns the next Node (next key value larger)
-        """
-        # If has right child, select, then traverse left all the way down
-        if self.right_child is not None:
-            node = self.right_child
-            while node.left_child is not None:
-                node = node.left_child
-            return node
-
-        node = self
-        # Try to find an ancestor that is a left child, return parent of that
-        while node.parent is not None:
-            if node.parent.left_child == node:
-                return node.parent
-            node = node.parent
-
-        # Nothing greater than this
-        return None
-
-    def previous(self):
-        """ Returns the previous Node (next key value smaller)
-        """
-        # If has left child, select, then traverse right all the way down
-        if self.left_child is not None:
-            node = self.left_child
-            while node.right_child is not None:
-                node = node.right_child
-            return node
-
-        node = self
-        # Try to find an ancestor that is a right child, return parent of that
-        while node.parent is not None:
-            if node.parent.right_child == node:
-                return node.parent
-            node = node.parent
-
-        #  Nothing smaller than this
-        return None
-
-    def is_leaf(self):
-        """ Return True if Leaf, False Otherwise
-        """
-        return self.height == 0
-
-    def max_child_height(self):
-        """ Return Height Of Tallest Child or -1 if No Children
-        """
-        if self.left_child and self.right_child:
-            # two children
-            return max(self.left_child.height, self.right_child.height)
-        elif self.left_child is not None and self.right_child is None:
-            # one child, on left
-            return self.left_child.height
-        elif self.left_child is None and self.right_child is not None:
-            # one child, on right
-            return self.right_child.height
-        else:
-            # no Children
-            return -1
-
-    def weigh(self):
-        """ Return How Left or Right Sided the Tree Is
-        Positive Number Means Left Side Heavy, Negative Number Means Right Side Heavy
-        """
-        if self.left_child is None:
-            left_height = -1
-        else:
-            left_height = self.left_child.height
-
-        if self.right_child is None:
-            right_height = -1
-        else:
-            right_height = self.right_child.height
-
-        balance = left_height - right_height
-        return balance
-
-    def update_height(self):
-        """ Updates Height of This Node and All Ancestor Nodes, As Necessary
-        """
-        node = self
-        while node is not None:
-            node.height = node.max_child_height() + 1
-            node = node.parent
-
-    def root(self):
-        node = self
-        while node.parent is not None:
-            node = node.parent
-        return node
-
-    def balance(self, tree):
-        """ Balances node, sets new tree root if appropriate
-        Note: If balancing does occur, this node will move to a lower position on the tree
-        """
-        while self.weigh() < -1 or self.weigh() > 1:
-            if self.weigh() < 0:
-                # right side heavy
-                if self.right_child.weigh() > 0:
-                    # right-side left-side heavy
-                    self.right_child.rotate_left()
-                # right-side right-side heavy
-                new_top = self.rotate_right()
-            else:
-                # left side heavy
-                if self.left_child.weigh() < 0:
-                    # left-side right-side heavy
-                    self.left_child.rotate_right()
-                # left-side left-side heavy
-                new_top = self.rotate_left()
-
-            if new_top.parent is None:
-                tree.root = new_top
-
-    def rotate_right(self):
-        assert(self.right_child is not None)
-        to_promote = self.right_child
-        swapper = to_promote.left_child
-
-        # swap children
-        self.right_child = swapper
-        to_promote.left_child = self
-        new_top = self._swap_parents(to_promote, swapper)
-        if swapper is not None:
-            swapper.update_height()
-        self.update_height()
-        return new_top
-
-    def rotate_left(self):
-        assert(self.left_child is not None)
-        to_promote = self.left_child
-        swapper = to_promote.right_child
-
-        # swap children
-        self.left_child = swapper
-        to_promote.right_child = self
-        new_top = self._swap_parents(to_promote, swapper)
-        if swapper is not None:
-            swapper.update_height()
-        self.update_height()
-        return new_top
-
-    def _swap_parents(self, promote, swapper):
-        """ re-assign parents, returns new top
-        """
-        promote.parent = self.parent
-        self.parent = promote
-        if swapper is not None:
-            swapper.parent = self
-
-        if promote.parent is not None:
-            if promote.parent.right_child == self:
-                promote.parent.right_child = promote
-            elif promote.parent.left_child == self:
-                promote.parent.left_child = promote
-        return promote
+        return self.table.numbers > other.table.numbers
 
     def genSuccessors(self):
         def flatten(matrix):
@@ -346,7 +160,8 @@ class Node():
                     swap(deepcopy(fatherMatrix), [3, 3], [2, 3]))
         for k in childsMatrix:
             newTable = table(k)
-            childNodes.append(Node(table=newTable, gValue=self.gValue))
+            childNodes.append(
+                Node(table=newTable, gValue=self.gValue + 1, realParent=self))
         return childNodes
 
     def h1(self):
